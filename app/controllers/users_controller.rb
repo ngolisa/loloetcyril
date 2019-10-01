ed45feb5fd4ccf4d84b0fa8ba7e79ff08ecfde5a
Lisa Ngo <lisango.e@gmail.com>
2019-10-02 01:52:27 +0200

    nuit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    group = Group.create
+    @user.group = group
+    if @user.save
+      redirect_to invites_path
+    else
+      render 'new'
+    end
+  end
+
   def invites
-    @users=User.all
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :phone, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   get 'user/invites'
   get '/invites', to: "users#invites"
-  devise_for :users
   root to: 'pages#home'
   resources :reservations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
